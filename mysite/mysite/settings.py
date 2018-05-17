f2391f19ed892a55e69cf6071e722279bc118ad8
iamdsc <dilpreetchawla382@gmail.com>
2018-05-17 15:54:37 +0530

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-	'annotatorapp.apps.AnnotatorappConfig',
+    'annotatorapp.apps.AnnotatorappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'table'
 ]
 
 MIDDLEWARE = [
