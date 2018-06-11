fc2e4162d207f224fa1464177b9bcb5d8420ef40
Sruti <shruti.samvabi@gmail.com>
2018-06-11 23:50:28 +0530

    few more comment statements added

--- mysite/annotatorapp/apps.py
+++ mysite/annotatorapp/apps.py
@@ -1,3 +1,5 @@
+#This file is created to help the user include any application configuration for the app.
+#Using this, you can configure some of the attributes of the application.
 from django.apps import AppConfig
 
 
