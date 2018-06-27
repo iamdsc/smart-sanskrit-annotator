852f53ecced4abe76d81b857a44b5ed03fed606a
iamdsc <dilpreetchawla382@gmail.com>
2018-06-28 01:20:40 +0530

    populated db with example sentences

--- mysite/annotatorapp/management/commands/scrap2.py
+++ mysite/annotatorapp/management/commands/scrap2.py
@@ -1,59 +1,28 @@
 import os
-import pickle
-import romtoslp as conv
+from django.core.management.base import BaseCommand
+from annotatorapp.models import Exsentences
 
-#In case of any clarification, email to - amrith@iitkgp.ac.in
-# The latest modifications can be found at: www.amrithkrishna.com 
-# Class definition for pickle files
-# currently the pickle files can be opened only with Python 3
 
-cur_dir = os.path.dirname(os.path.realpath(__file__))
-dir_p = os.path.join(cur_dir,'pickle/')
-dir_graphml = os.path.join(cur_dir,'graphml/')
+dirname = os.path.dirname(__file__)
+path = os.path.join(dirname,'answers.txt')
 
-class DCS:
-    def __init__(self, sent_id, sentence):
-        self.sent_id = sent_id
-        self.sentence = sentence
-        self.dcs_chunks = []
-        self.lemmas = []
-        self.cng = []
+class Command(BaseCommand):
+	help = 'Populates the database with example sentences and their solution'
+	    
+	def extract(self):
+		f = open(path,encoding='utf-8')
+		for l in f.readlines():
+			c=l.split('#')
+			line = c[0].rstrip()
+			chunks = c[1]
+			lemmas = c[2]
+			morph_cng = c[3][:-1]
+			try:
+				xsent = Exsentences(line=line,chunks=chunks,lemmas=lemmas,morph_cng=morph_cng)
+				xsent.save()
+			except Exception as e:
+				print(e)
+		f.close()			
 
-node = {'word':'', 'morph':'', 'morph_no':'',};
-new_cng = []
-_out = open('answers.txt','w',encoding='utf8')
-
-for filename in os.listdir(dir_p):
-    if('.p' in filename):
-        output_load = pickle.load(open(dir_p+filename, "rb"), encoding='utf-8')
-        lemmas = sum(output_load.lemmas,[])
-        for i in range(len(lemmas)):
-            lemmas[i] = conv.rom_slp(lemmas[i])
-        
-        cng = sum(output_load.cng,[])
-        g_file = dir_graphml+filename[:-1]+'txt'
-        print(g_file)
-        nodes = []
-        with open(g_file,'r') as f:
-            for line in f:
-                if('edge source' in line):
-                    break
-                if('data key=\"d0\"' in line):
-                    node['morph_no'] = line.replace('<','>').split('>')[2]
-                if('data key=\"d4\"' in line):
-                    node['word'] = line.replace('<','>').split('>')[2]
-                if('data key=\"d5\"' in line):
-                    node['morph'] = line.replace('<','>').split('>')[2]
-                if('/node' in line):
-                    nodes.append(node.copy())    
-        nodes = [dict(t) for t in set([tuple(d.items()) for d in nodes])]
-        
-        # compare 
-        for lemma,_cng in zip(lemmas,cng):
-            for cdict in nodes:
-                if lemma == cdict['word'] and _cng == cdict['morph_no']:
-                    new_cng.append(cdict['morph'])
-        _out.write(str(output_load.sentence)+'#'+str(output_load.dcs_chunks)+'#'+str(lemmas)+'#'+str(new_cng)+'\n')
-        new_cng.clear()
-
-_out.close()
+	def handle(self, *args, **options):
+		self.extract()	
