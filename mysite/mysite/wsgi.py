a5fb80997727fe74e5e7c03ad2a12146567035cd
svidhyut <vidhyut97@yahoo.co.in>
2018-07-03 10:39:53 +0000

    saved modifications post setup (with proxies, relative paths and "'" replacement

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
